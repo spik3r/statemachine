985b03254b6e1c70ef325c2b8c178273d351e3dd
kai tait <kai-tait@macolighting.com>
2018-05-01 21:51:02 +0200

    Messing around with states and redirects

--- src/main/java/com/kaitait/statemachine/CheckoutController.java
+++ src/main/java/com/kaitait/statemachine/CheckoutController.java
@@ -9,6 +9,9 @@ import org.springframework.statemachine.StateMachine;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class CheckoutController {
 
@@ -18,8 +21,9 @@ public class CheckoutController {
     private static final Logger LOG = LoggerFactory.getLogger(CheckoutController.class);
 
     @RequestMapping(path = "checkout/basket")
-    public void basket() {
+    public String basket() {
         LOG.info("basket");
+        LOG.info(String.valueOf("state: " + stateMachine.getState().getId().order));
         // Two ways to send events the quick way with sendEvent or sendEvent with a message incase you need headers etc
 //        stateMachine.sendEvent(Events.BASKET_CREATED);
         Message<Events> eventsMessage = MessageBuilder
@@ -27,35 +31,75 @@ public class CheckoutController {
                 .setHeader("header_foo", "bar")
                 .build();
         stateMachine.sendEvent(eventsMessage);
+        return "basket";
     }
 
     @RequestMapping(path = "checkout/address")
-    public void address() {
+    public String address(HttpServletResponse response) throws IOException {
         LOG.info("address");
+//        response.sendRedirect("basket");
+//        LOG.info(String.valueOf(Pages.get(1)));
+//        LOG.info(String.valueOf(Pages.get(2)));
+        LOG.info(String.valueOf("state: " + stateMachine.getState().getId().order));
+        if (!isOnCorrectPage(stateMachine.getState().getId(), Pages.ADDRESS)) {
+            return redirectIfOnWrongPage(stateMachine.getState().getId(), Pages.ADDRESS, response);
+        }
         stateMachine.sendEvent(Events.BASKET_CREATED);
+        return "address";
     }
 
     @RequestMapping(path = "checkout/timeslot")
-    public void timeslot() {
+    public String timeslot(HttpServletResponse response) throws IOException {
         LOG.info("timeslot");
+        LOG.info(String.valueOf("state: " + stateMachine.getState().getId().order));
+        if (!isOnCorrectPage(stateMachine.getState().getId(), Pages.TIMESLOT)) {
+            return redirectIfOnWrongPage(stateMachine.getState().getId(), Pages.TIMESLOT, response);
+        }
+        redirectIfOnWrongPage(stateMachine.getState().getId(), Pages.TIMESLOT, response);
         stateMachine.sendEvent(Events.ADDRESS_SELECTED);
+        return "timeslot";
     }
 
     @RequestMapping(path = "checkout/payment")
-    public void payment() {
+    public String payment() {
         LOG.info("payment");
+        LOG.info(String.valueOf("state: " + stateMachine.getState().getId().order));
         stateMachine.sendEvent(Events.TIMESLOT_SELECTED);
+        return "payment";
     }
 
     @RequestMapping(path = "checkout/confirmation")
-    public void confirmation() {
+    public String confirmation() {
         LOG.info("confirmation");
         stateMachine.sendEvent(Events.PAYMENT_METHOD_SELECTED);
+        return "confirmation";
     }
 
     @RequestMapping(path = "checkout/aftersale")
-    public void aftersale() {
+    public String aftersale() {
         LOG.info("aftersale");
+        LOG.info(String.valueOf("state: " + stateMachine.getState().getId().order));
         stateMachine.sendEvent(Events.ORDER_CONFIRMED);
+        return "aftersale";
+    }
+
+    private static boolean isOnCorrectPage(Pages current, Pages previous) {
+        LOG.info("oder: " + (previous.order - 1 < current.order));
+        LOG.info("oder: " + (previous.order));
+        LOG.info("oder: " + (current.order));
+        if (current.order - 1 < previous.order) {
+            return false;
+        }
+        return true;
+    }
+
+    private static String redirectIfOnWrongPage(Pages current, Pages previous, HttpServletResponse response) throws IOException {
+        if (!isOnCorrectPage(current, previous)) {
+            LOG.info("redirect");
+            final String redirectUrl = previous.toString().toLowerCase();
+            response.sendRedirect(redirectUrl);
+            return redirectUrl;
+        }
+        return "";
     }
 }
--- src/main/java/com/kaitait/statemachine/Pages.java
+++ src/main/java/com/kaitait/statemachine/Pages.java
@@ -1,5 +1,10 @@
 package com.kaitait.statemachine;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 public enum Pages {
     BASKET(1),
     ADDRESS(2),
@@ -12,5 +17,26 @@ public enum Pages {
 
     private Pages(int order) {
         this.order = order;
+    }
+
+    public static Pages find(int page, Supplier<? extends Pages> byDef) {
+        return Arrays.asList(Pages.values()).stream()
+                .filter(e -> e.order == page).findFirst().orElseGet(byDef);
+    }
+
+    private static final Map lookup =
+            new HashMap();
+    static {
+        //Create reverse lookup hash map
+        for(Pages page : Pages.values())
+            lookup.put(page.getOrder(), page);
+    }
+
+    public int getOrder() { return order; }
+
+    public static Pages get(int order) {
+        //the reverse lookup by simply getting
+        //the value from the lookup HsahMap.
+        return (Pages) lookup.get(order);
     }
 }
